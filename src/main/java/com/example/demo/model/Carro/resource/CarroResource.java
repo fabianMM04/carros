33fb90aeec5bddea5a613eb099a18e13eb35ef26
FABIAN MARTINEZ MARTELO <fabianmm04@gmail.com>
2019-05-30 01:28:50 -0500

    CRUD carro

---
+++ src/main/java/com/example/demo/model/Carro/resource/CarroResource.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.model.Carro.resource;
+
+import com.example.demo.model.Carro.model.Carro;
+import com.example.demo.model.Carro.service.CarroService;
+import java.util.List;
+import javax.validation.Valid;
+import org.apache.tomcat.util.http.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Simon
+ */
+@RestController
+@RequestMapping("/Carro")
+public class CarroResource {
+
+    @Autowired
+    private CarroService carroService;
+
+    @RequestMapping(value = "getAllCarro", method = RequestMethod.GET)
+    public List<Carro> findAllCarro() {
+        return carroService.consultarAllCarro();
+    }
+
+    @RequestMapping(value = "getCarroById/{id}", method = RequestMethod.GET)
+    public Carro findCarroById(@PathVariable Long id) {
+        return carroService.consultarCarroById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void guardarRol(@RequestBody @Valid Carro carro) {
+        carroService.guardarCarro(carro);
+    }
+    
+    @RequestMapping(value = "updateCarroById/{id}", method = RequestMethod.POST)
+    public void updateCarroById(@PathVariable Long id, @Valid Carro carro) {
+         carroService.actualizarCarro(id, carro);
+    }
+
+    @RequestMapping(value = "deleteCarro/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCarroById(@PathVariable Long id) {
+         carroService.eliminarCarro(id);
+    }
+}
